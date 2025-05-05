25113eee6d3455a061be8c7b7c31af1a576c7e1a
Gabriel Salas <gbss91@gmail.com>
2025-05-05 21:37:48 +0100

    🛠 Add types

---
+++ src/types.ts
@@ -0,0 +1,30 @@
+// Custom types
+
+export type Property = {
+  id: string;
+  address1: string;
+  address2?: string;
+  address3?: string;
+  address4?: string;
+  address5?: string;
+  county: string;
+  eircode: string;
+  bedroomNo: number;
+  isRegistered?: boolean;
+  rentPrices: Rent;
+  reviews?: Review;
+};
+
+export type Rent = {
+  id: string;
+  rentValue: number;
+  rentPeriod: string;
+  propertyId?: number;
+};
+
+export type Review = {
+  id: string;
+  rating: number;
+  review?: string;
+  propertyId?: number;
+};
