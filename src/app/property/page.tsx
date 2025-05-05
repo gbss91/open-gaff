8a16594d2544405712562b5345fcf44a30c31a6d
Gabriel Salas <gbss91@gmail.com>
2025-05-05 20:07:10 +0100

    🎨 Change search bar width

--- src/app/property/page.tsx
+++ src/app/property/page.tsx
@@ -3,10 +3,12 @@ import SearchBar from "../components/SearchBar";
 export default function PropertyPage() {
   return (
     <main style={{ minHeight: "calc(100vh - 64px)" }}>
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <SearchBar />
+      <div className="max-w-screen-xl flex flex-wrap items-center mx-auto p-4">
+        <div className="w-full">
+          <SearchBar />
+        </div>
       </div>
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"></div>
+      <div className="max-w-screen-xl flex flex-wrap items-center mx-auto p-4"></div>
     </main>
   );
 }
