1c55a28e852fad5c0ab84e30d7fc686ff6721247
Gabriel Salas <gbss91@gmail.com>
2025-05-05 17:06:29 +0100

    🎨 Added main page style

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className="bg-white border-gray-200 dark:bg-gray-900 h-[80px]">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link
           href="/"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
-"use client";
-
-import Image from "next/image";
+import MainSearch from "./components/MainSearch";
 
 export default function MainPage() {
-  return <></>;
+  return (
+    <main
+      className="flex items-center justify-center"
+      style={{ minHeight: "calc(100vh - 80px)" }}
+    >
+      <div className="w-full max-w-md px-4">
+        <h2 className="text-3xl font-bold mb-6 text-center">
+          Search Properties
+        </h2>
+        <MainSearch />
+      </div>
+    </main>
+  );
 }
