2a8255bc3054efae7469b6d9ebc1db552472d43c
Gabriel Salas <gbss91@gmail.com>
2025-05-05 23:24:22 +0100

    🛠 Add route

--- src/app/property/page.tsx
+++ src/app/property/page.tsx
@@ -1,7 +1,22 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import SearchBar from "../components/SearchBar";
 import PropertyList from "./components/PropertyList";
+import { Property } from "@/types";
 
 export default function PropertyPage() {
+  const [properties, setProperties] = useState<Property[]>([]);
+
+  useEffect(() => {
+    fetch("/api/properties")
+      .then((res) => res.json())
+      .then((data: Property[]) => setProperties(data))
+      .catch((error) => console.error("Error fetching properties:", error));
+  }, []);
+
+  console.log(properties);
+
   return (
     <main style={{ minHeight: "calc(100vh - 64px)" }}>
       <div className="max-w-screen-xl flex flex-wrap items-center mx-auto p-4">
