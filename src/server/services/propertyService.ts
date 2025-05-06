613a2096a48ac07fcda009db3350ff17ed85424b
Gabriel Salas <gbss91@gmail.com>
2025-05-06 17:20:59 +0100

    🛠 Add service to get properties by ID

--- src/server/services/propertyService.ts
+++ src/server/services/propertyService.ts
@@ -1,5 +1,20 @@
 import prisma from "../prisma";
 
-export const getAllProperties = async () => {
-  return prisma.property.findMany();
+export const propertyService = {
+  getAllProperties: async () => {
+    return await prisma.property.findMany();
+  },
+
+  getPropertiesByQuery: async () => {
+    //Todo
+  },
+
+  getPropertyById: async (id: number) => {
+    const property = await prisma.property.findUnique({ where: { id: id } });
+    return property;
+  },
+
+  addProperty: async () => {
+    //TODO
+  },
 };
