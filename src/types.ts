bfc2467e564dec802487dcc42e31ad536b819015
Gabriel Salas <gbss91@gmail.com>
2025-05-06 21:30:24 +0100

    🛠 Update types

--- src/types.ts
+++ src/types.ts
@@ -3,28 +3,28 @@
 export type Property = {
   id: string;
   address1: string;
-  address2?: string;
-  address3?: string;
-  address4?: string;
-  address5?: string;
+  address2?: string | null;
+  address3?: string | null;
+  address4?: string | null;
+  address5?: string | null;
   county: string;
   eircode: string;
   bedroomNo: number;
-  isRegistered?: boolean;
-  rentPrices?: Rent;
-  reviews?: Review;
+  isRegistered?: boolean | null;
+  rentPrices?: Rent | null;
+  reviews?: Review | null;
 };
 
 export type Rent = {
   id: string;
   rentValue: number;
   rentPeriod: string;
-  propertyId?: number;
+  propertyId?: number | null;
 };
 
 export type Review = {
   id: string;
   rating: number;
-  review?: string;
-  propertyId?: number;
+  review?: string | null;
+  propertyId?: number | null;
 };
