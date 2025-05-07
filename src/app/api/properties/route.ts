1f0f9221c78a35dfeebd5bdb77ffad81967f496a
Gabriel Salas <gbss91@gmail.com>
2025-05-07 21:53:46 +0100

    🚏 Add route to create property

--- src/app/api/properties/route.ts
+++ src/app/api/properties/route.ts
@@ -1,12 +1,28 @@
 import { propertyService } from "@/server/services/propertyService";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
+// Get all properties
 export const GET = async () => {
   try {
     const properties = await propertyService.getAllProperties();
     return NextResponse.json(properties);
   } catch (error) {
-    console.error(error);
+    console.error("API Error: ", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+};
+
+// Create a property
+export const POST = async (request: NextRequest) => {
+  try {
+    const body = await request.json();
+    const newProperty = await propertyService.addProperty(body);
+    return NextResponse.json(newProperty, { status: 201 });
+  } catch (error) {
+    console.error("API Error: ", error);
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
