567ed765f8b320a19cd9c3472c78759376eba793
Gabriel Salas <gbss91@gmail.com>
2025-05-08 23:52:40 +0100

    🎨 Add dynamic map

--- src/app/property/[id]/page.tsx
+++ src/app/property/[id]/page.tsx
@@ -1,17 +1,9 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import DynamicMap from "@/app/components/DynamicMap";
 
 export default function PropertyPage() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <>
+      <DynamicMap />
+    </>
   );
 }
