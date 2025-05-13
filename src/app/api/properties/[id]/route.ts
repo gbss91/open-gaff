676cf952e29854546a548ac44c985fb8bb7c75a9
Gabriel Salas <gbss91@gmail.com>
2025-05-13 20:47:16 +0100

    🚏 Add get property by id route

---
+++ src/app/api/properties/[id]/route.ts
@@ -0,0 +1,30 @@
+import { propertyService } from "@/server/services/propertyService";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) => {
+  try {
+    const { id } = await params;
+    const property = propertyService.getPropertyById(parseInt(id), [
+      "reviews",
+      "rents",
+    ]);
+
+    if (!property) {
+      return NextResponse.json(
+        { error: "Property not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(property);
+  } catch (error) {
+    console.error("API Error: ", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+};
