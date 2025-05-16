b9cd72c0c6ccfc2ce23a681408e4e603c5b7ea7c
Gabriel Salas <gbss91@gmail.com>
2025-05-16 19:12:47 +0100

    🛠 Update test config

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,15 @@ import type { Config } from "jest";
 
 const config: Config = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   roots: ["<rootDir>/src", "<rootDir>/src/tests"],
-  moduleFileExtensions: ["ts", "js", "json"],
+  moduleFileExtensions: ["ts", "tsx", "js", "json"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
   },
 };
 
