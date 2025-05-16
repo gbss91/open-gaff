8d4bc37832c43b9d6b0c54f5ee3693239c069564
Gabriel Salas <gbss91@gmail.com>
2025-05-16 18:09:37 +0100

    🧪 Update service tests

--- src/tests/unit/propertyService.test.ts
+++ src/tests/unit/propertyService.test.ts
@@ -2,7 +2,7 @@ import { prismaMock } from "@/tests/mocks/prisma";
 import { propertyService } from "@/server/services/propertyService";
 
 describe("Property service unit tests", () => {
-  test("getAllProperties returns properties", async () => {
+  test("getAllProperties returns properties without relations", async () => {
     const mockProperties = [
       {
         id: 1,
@@ -15,12 +15,50 @@ describe("Property service unit tests", () => {
     ];
     prismaMock.property.findMany.mockResolvedValue(mockProperties);
 
-    const result = await propertyService.getAllProperties();
-    expect(prismaMock.property.findMany).toHaveBeenCalled();
+    const result = await propertyService.getAllProperties([], 1);
+    expect(prismaMock.property.findMany).toHaveBeenCalledWith({
+      skip: 0,
+      take: 5,
+      include: {
+        reviews: false,
+        rents: false,
+      },
+    });
     expect(result).toEqual(mockProperties);
   });
 
-  test("getPropertyById returns a property", async () => {
+  test("getAllProperties returns properties with relations", async () => {
+    const mockPropertiesWithRelations = [
+      {
+        id: 1,
+        address1: "Test",
+        county: "Dublin",
+        eircode: "D08",
+        bedroomNo: 2,
+        isRegistered: false,
+        reviews: [],
+        rents: [],
+      },
+    ];
+
+    prismaMock.property.findMany.mockResolvedValue(mockPropertiesWithRelations);
+
+    const result = await propertyService.getAllProperties(
+      ["reviews", "rents"],
+      1
+    );
+    expect(prismaMock.property.findMany).toHaveBeenCalledWith({
+      skip: 0,
+      take: 5,
+      include: {
+        reviews: true,
+        rents: true,
+      },
+    });
+    expect(result).toEqual(mockPropertiesWithRelations);
+  });
+
+  test("getPropertyById returns a property without reviews", async () => {
     const mockProperty = {
       id: 1,
       address1: "Test",
@@ -29,9 +67,13 @@ describe("Property service unit tests", () => {
     };
     prismaMock.property.findUnique.mockResolvedValue(mockProperty);
 
-    const result = await propertyService.getPropertyById(1);
+    const result = await propertyService.getPropertyById(1, []);
     expect(prismaMock.property.findUnique).toHaveBeenCalledWith({
       where: { id: 1 },
+      include: {
+        reviews: false,
+        rents: false,
+      },
     });
     expect(result).toEqual(mockProperty);
   });
@@ -55,18 +97,28 @@ describe("Property service unit tests", () => {
     expect(result).toEqual(mockCreated);
   });
 
-  test("getPropertiesByQuery calls findMany with OR conditions", async () => {
+  test("getPropertiesByQuery calls findMany with OR conditions and reviews", async () => {
     const mockResult = [
       { id: 2, address1: "Gran Street", county: "Galway", isRegistered: false },
     ];
     prismaMock.property.findMany.mockResolvedValue(mockResult);
 
     const query = "Grand";
-    const result = await propertyService.getPropertiesByQuery(query);
+    const result = await propertyService.getPropertiesByQuery(
+      query,
+      ["reviews"],
+      1
+    );
 
     expect(prismaMock.property.findMany).toHaveBeenCalledWith({
+      skip: 0,
+      take: 5,
       where: {
         OR: expect.any(Array),
+      },
+      include: {
+        reviews: true,
+        rents: false,
       },
     });
     expect(result).toEqual(mockResult);
