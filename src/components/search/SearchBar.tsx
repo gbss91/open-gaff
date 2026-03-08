3aae2965a48b99b4f02d5f328d104fd728f776f9
Gabriel Salas <gbss91@gmail.com>
2026-03-08 16:27:03 +0000

    Add onSearch and change style

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -3,35 +3,34 @@ import styles from "./SearchBar.module.css";
 import { Search } from "lucide-react";
 
 type SearchBarProps = {
+  onSearch: (formData: FormData) => void | Promise<void>;
   className?: string;
 };
 
-const SearchBar = ({ className }: SearchBarProps) => {
+const SearchBar = ({ onSearch, className }: SearchBarProps) => {
   return (
-    <div>
-      <Form action="/property" className={className}>
-        <div className={`${styles.searchContainer} relative`}>
-          <Search
-            name="search"
-            size="18"
-            className={`${styles.searchIcon} absolute left-2.5 top-1/2 -translate-y-1/2`}
-          />
-          <input
-            type="search"
-            id="search-bar"
-            name="search"
-            className={`${styles.input} bg-white text-sm block w-full px-8 py-4`}
-            placeholder="Search address or Eircode..."
-          />
-          <button
-            type="submit"
-            className={`${styles.searchBtn} bg-primary absolute right-2.5 top-1/2 -translate-y-1/2 text-white text-sm font-bold px-4 py-2`}
-          >
-            Search
-          </button>
-        </div>
-      </Form>
-    </div>
+    <Form action={onSearch} className={className}>
+      <div className="relative">
+        <Search
+          name="search"
+          size="18"
+          className="stroke-muted absolute left-2.5 top-1/2 -translate-y-1/2"
+        />
+        <input
+          type="search"
+          id="search-bar"
+          name="search"
+          className={`${styles.input} bg-white text-sm block w-full px-8 py-4`}
+          placeholder="Search by address or Eircode..."
+        />
+        <button
+          type="submit"
+          className={`${styles.searchBtn} bg-primary absolute right-2.5 top-1/2 -translate-y-1/2 text-white text-sm px-4 py-2`}
+        >
+          Search
+        </button>
+      </div>
+    </Form>
   );
 };
 
