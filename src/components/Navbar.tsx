4b2a80ef297afa2b082ac48dda84e51d546b18d9
Gabriel Salas <gbss91@gmail.com>
2026-03-08 21:59:16 +0000

    Modify nav menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,14 +5,10 @@ import { House } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 import Button from "./ui/Button";
-import { Plus } from "lucide-react";
+import { Plus, Menu } from "lucide-react";
 
 const navItems = [
-  {
-    id: "properties",
-    label: "Properties",
-    href: "/properties",
-  },
+  { id: "properties", label: "Properties", href: "/properties" },
 ];
 
 const Navbar = () => {
@@ -22,7 +18,7 @@ const Navbar = () => {
   return (
     <nav className="bg-primary h-16" data-testid="nav-container">
       <div
-        className="max-w-full flex flex-wrap items-center justify-between mx-auto p-4"
+        className="max-w-full flex items-center justify-between mx-auto px-4 h-full"
         data-testid="nav-logo"
       >
         <Link
@@ -34,36 +30,10 @@ const Navbar = () => {
             OpenGaff
           </span>
         </Link>
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-20"
-          onClick={() => setMenuOpen((prev) => !prev)}
-          data-testid="nav-menu-btn"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
         <div
-          className={`w-full md:block md:w-auto ${
-            menuOpen ? "block" : "hidden"
-          }`}
+          className={`w-full md:block md:w-auto ${menuOpen ? "nav-menu-open block" : "hidden"}`}
         >
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
+          <ul className="flex flex-col text-text-dark md:flex-row md:items-center md:gap-8 md:text-white">
             {navItems.map((item) => {
               const isActive = pathname === item.href;
 
@@ -71,10 +41,10 @@ const Navbar = () => {
                 <li key={item.id}>
                   <Link
                     href={item.href}
-                    className={`block py-2 px-3 md:p-0 border-b-3 text-gray-700 ${
+                    className={`block py-2 px-3 border-b-2 md:p-0 ${
                       isActive
-                        ? "border-blue-700"
-                        : "border-transparent hover:border-gray-300"
+                        ? "border-accent"
+                        : "border-transparent hover:border-white"
                     }`}
                   >
                     {item.label}
@@ -82,15 +52,26 @@ const Navbar = () => {
                 </li>
               );
             })}
+            <li>
+              <Button
+                btnType="secondary"
+                size="sm"
+                icon={<Plus />}
+                text="Add Rent"
+                testId="add-rent-btn"
+              />
+            </li>
           </ul>
         </div>
-        <Button
-          btnType="secondary"
-          size="sm"
-          icon={<Plus />}
-          text="Add Rent"
-          testId="add-rent-btn"
-        />
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center p-1 w-8 h-8 justify-center text-sm text-accent rounded-lg md:hidde focus:outline-none"
+          onClick={() => setMenuOpen((prev) => !prev)}
+          data-testid="nav-menu-btn"
+        >
+          <Menu size="24" className="stroke-accent hover:stroke-accent-hover" />
+        </button>
       </div>
     </nav>
   );
