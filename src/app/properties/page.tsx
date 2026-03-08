184bc6fac43eddf0d24658957c0a0c842551c816
Gabriel Salas <gbss91@gmail.com>
2026-03-08 17:51:33 +0000

    Fix issue

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -5,12 +5,12 @@ import SearchBar from "@/components/search/SearchBar";
 export default function PropertiesPage() {
   return (
     <main style={{ minHeight: "calc(100vh - 64px)" }}>
-      <div className="max-w-screen-xl flex flex-wrap items-center mx-auto p-4">
+      <div className="max-w-xl flex flex-wrap items-center mx-auto p-4">
         <div className="w-full">
-          <SearchBar />
+          <SearchBar action="/properties" />
         </div>
       </div>
-      <div className="max-w-screen-xl flex flex-wrap items-center mx-auto p-4">
+      <div className="max-w-xl flex flex-wrap items-center mx-auto p-4">
         <div className="w-full">
           <Suspense>
             <PropertyList />
