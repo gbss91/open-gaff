6e6e98ba99a458dd8be33313a7cbfa32900737be
Gabriel Salas <gbss91@gmail.com>
2026-03-08 17:29:47 +0000

    Add tests

---
+++ src/tests/integration/components/searchBar.test.ts
@@ -0,0 +1,31 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+
+describe("Integration | SearchBar", () => {
+  beforeEach(() => {
+    render(<SearchBar action="/property" />);
+  });
+
+  it("renders the search input", () => {
+    expect(
+      screen.getAllByTestId("search-bar")
+    ).toBeInTheDocument();
+  });
+
+  it("renders the search correct icon", () => {
+    expect(
+      screen.getByTestId("search-bar-icon")
+    ).toBeInTheDocument();
+  });
+
+  it("renders the search button", () => {
+    expect(
+      screen.getByTestId("search-bar-button")
+    ).toBeInTheDocument();
+  });
+
+  it("accepts user input", () => {
+    const input = screen.getByRole("searchbox");
+    fireEvent.change(input, { target: { value: "Dublin" } });
+    expect(input).toHaveValue("Dublin");
+  });
+});
