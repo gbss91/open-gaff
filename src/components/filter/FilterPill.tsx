dd2e9983adc3754c634c2b8816e8cfd62eb8e5b7
Gabriel Salas <gbss91@gmail.com>
2026-03-09 21:54:25 +0000

    Add icon position

--- src/components/filter/FilterPill.tsx
+++ src/components/filter/FilterPill.tsx
@@ -3,19 +3,27 @@ import syles from "./filterBar.module.css";
 type FilterPillProps = {
   label: string;
   icon?: React.ReactNode;
+  iconPosition?: "left" | "right";
   active?: boolean;
   onClick?: () => void;
 };
 
-const FilterPill = ({ label, icon, active, onClick }: FilterPillProps) => {
+const FilterPill = ({
+  label,
+  icon,
+  iconPosition = "left",
+  active,
+  onClick,
+}: FilterPillProps) => {
   return (
     <div
       className={`${syles["filter-pill"]} ${active ? syles["active"] : ""} inline-flex items-center text-sm font-medium px-3`}
       onClick={onClick}
       data-testid="filter-pill"
     >
-      {icon && icon}
+      {icon && iconPosition === "left" && icon}
       {label}
+      {icon && iconPosition === "right" && icon}
     </div>
   );
 };
