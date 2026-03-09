a9abb3fd5bb27ca840ed900b6a6166aac140675b
Gabriel Salas <gbss91@gmail.com>
2026-03-09 19:26:25 +0000

    Define HTML structure

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -4,17 +4,23 @@ import SearchBar from "@/components/search/SearchBar";
 
 export default function PropertiesPage() {
   return (
-    <main style={{ minHeight: "calc(100vh - 64px)" }}>
-      <div className="max-w-xl flex flex-wrap items-center mx-auto p-4">
-        <div className="w-full">
-          <SearchBar action="/properties" />
-        </div>
+    <main className="flex-1 flex flex-col">
+      <div className="py-4 px-main" data-testid="search-container">
+        <SearchBar action="/properties" />
       </div>
-      <div className="max-w-xl flex flex-wrap items-center mx-auto p-4">
-        <div className="w-full">
-          <Suspense>
-            <PropertyList />
-          </Suspense>
+      <div className="filter-container px-main">S</div>
+      <div className="flex flex-1 flex-row">
+        <div
+          className="list-panel bg-accent flex-1 pl-main"
+          data-testid="list-panel"
+        >
+          S
+        </div>
+        <div
+          className="map-panel bg-amber-500 flex-1 px-0"
+          data-testid="map-panel"
+        >
+          S
         </div>
       </div>
     </main>
