08bd0d91931c92a3bf2f58ab50854d3043faaca1
Gabriel Salas <gbss91@gmail.com>
2026-03-10 19:48:37 +0000

    🛠 Fix import

--- src/server/prisma.ts
+++ src/server/prisma.ts
@@ -1,6 +1,6 @@
+import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
-import { PrismaClient } from "../generated/prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
