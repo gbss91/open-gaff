8d8504638afb76a90570f0e69fc091993b1e0674
Gabriel Salas <gbss91@gmail.com>
2026-03-11 22:19:57 +0000

    ✅ Enable sorting and add map

--- src/components/map/DynamicMap.tsx
+++ src/components/map/DynamicMap.tsx
@@ -1,21 +1,18 @@
 "use client";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 const DynamicMap = () => {
   return (
     <MapContainer
       center={[53.3498, -6.2603]}
       zoom={13}
-      style={{ height: "400px", width: "100%" }}
+      style={{ height: "100%", width: "100%" }}
     >
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
       />
-      <Marker position={[53.3498, -6.2603]}>
-        <Popup>Dublin</Popup>
-      </Marker>
     </MapContainer>
   );
 };
