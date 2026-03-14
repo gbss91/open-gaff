ecf263b99bd1330ee54ac133855266fae6334a3f
Gabriel Salas <gbss91@gmail.com>
2026-03-14 19:27:10 +0000

    ❌ Comment out map

--- src/components/map/DynamicMap.tsx
+++ src/components/map/DynamicMap.tsx
@@ -1,20 +1,20 @@
-"use client";
-import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer } from "react-leaflet";
+// "use client";
+// import "leaflet/dist/leaflet.css";
+// import { MapContainer, TileLayer } from "react-leaflet";
 
-const DynamicMap = () => {
-  return (
-    <MapContainer
-      center={[53.3498, -6.2603]}
-      zoom={13}
-      style={{ height: "100%", width: "100%" }}
-    >
-      <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-      />
-    </MapContainer>
-  );
-};
+// const DynamicMap = () => {
+//   return (
+//     <MapContainer
+//       center={[53.3498, -6.2603]}
+//       zoom={13}
+//       style={{ height: "100%", width: "100%" }}
+//     >
+//       <TileLayer
+//         url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+//       />
+//     </MapContainer>
+//   );
+// };
 
-export default DynamicMap;
+// export default DynamicMap;
